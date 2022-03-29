7df6b9af3ca9f139cdec05d3d5ae309c54e8b516
aclalick <61526670+aclalick@users.noreply.github.com>
2022-03-29 17:25:22 +0000

    New Grid method started

--- src/logic.py
+++ src/logic.py
@@ -1,6 +1,5 @@
-import random
-from typing import List, Dict
 import numpy as np
+import threading
 """
 This file can be a nice home for your Battlesnake's logic and helper functions.
   
@@ -23,237 +22,19 @@ def get_info() -> dict:
         "tail": "ice-skate",
     }
 
-## A* implementation from https://medium.com/@nicholas.w.swift/easy-a-star-pathfinding-7e6689c7f7b2
-class Node():
-    """A node class for A* Pathfinding"""
-
-    def __init__(self, parent=None, position=None):
-        self.parent = parent
-        self.position = position
-
-        self.g = 0
-        self.h = 0
-        self.f = 0
-
-    def __eq__(self, other):
-        return self.position == other.position
-
-
-def findFood(maze, start, tail):
-    """Returns a list of tuples as a path from the given start to the given end in the given maze"""
-    # Create start and end node
-    
-    start_node = Node(None, (start['x'], start['y']))
-    start_node.g = start_node.h = start_node.f = 0
-    tailNode = None
-
-    # Initialize both open and closed list
-    open_list = []
-    closed_list = []
-    mazeSize = (len(maze), len(maze[0]))
-
-    # Add the start node
-    open_list.append(start_node)
-
-    # Loop until you find the end
-    while len(open_list) > 0 and len(closed_list) < 150:
-
-        # Get the current node
-        current_node = open_list[0]
-        current_index = 0
-        for index, item in enumerate(open_list):
-            if item.f < current_node.f:
-                current_node = item
-                current_index = index
-
-        # Pop current off open list, add to closed list
-        open_list.pop(current_index)
-        closed_list.append(current_node)
-
-        # Found a goal
-        if (current_node.position[0], current_node.position[1]) == (tail['x'], tail['y']):
-            tailNode = current_node
-        
-        if maze[current_node.position[0]][current_node.position[1]] == -1:
-            path = []
-            current = current_node
-            while current is not None:
-                path.append(current.position)
-                current = current.parent
-            return path[::-1] # Return reversed path
-
-        # Check Neighbors
-        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0)]: # Adjacent squares
-
-            # Get node position
-            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
-
-            # Make sure within range
-            if node_position[0] > (mazeSize[0] - 1) or node_position[0] < 0 or node_position[1] > (mazeSize[1] - 1) or node_position[1] < 0:
-                continue
-
-            # Make sure walkable terrain
-            if maze[node_position[0]][node_position[1]] - current_node.g > 0:
-                continue
-
-            # Create new node
-            child = Node(current_node, node_position)
-
-            if child in closed_list or child in open_list:
-                continue
-            
-            # Child is on the closed list
-            for closed_child in closed_list:
-                if child == closed_child:
-                    continue
-
-            # Create the f, g, and h values
-            child.g = current_node.g + 1
-            child.h = 0
-            child.f = child.g + child.h
-
-            # Child is already in the open list
-            for open_node in open_list:
-                if child == open_node and child.g >= open_node.g:
-                    continue
-
-            # Add the child to the open list
-            open_list.append(child)
-
-def _rem_dir(dir: str, possible_moves: List[str]) -> List[str]:
-    if dir in possible_moves:
-        if dir == "left":
-            possible_moves.remove("left")
-        elif dir == "right":
-            possible_moves.remove("right")
-        elif dir == "down":
-            possible_moves.remove("down")
-        elif dir == "up":
-            possible_moves.remove("up")
-            
-    return possible_moves   
-
-def _avoid_my_neck(my_body: dict, possible_moves: List[str]) -> List[str]:
-    """
-    my_body: List of dictionaries of x/y coordinates for every segment of a Battlesnake.
-            e.g. [{"x": 0, "y": 0}, {"x": 1, "y": 0}, {"x": 2, "y": 0}]
-    possible_moves: List of strings. Moves to pick from.
-            e.g. ["up", "down", "left", "right"]
-    
-    return: The list of remaining possible_moves, with the 'neck' direction removed
-    """
-    my_head = my_body[0]  # The first body coordinate is always the head
-    my_neck = my_body[1]  # The segment of body right after the head is the 'neck'
-    
-    if my_neck["x"] < my_head["x"]:  # my neck is left of my head
-        possible_moves.remove("left")
-    elif my_neck["x"] > my_head["x"]:  # my neck is right of my head
-        possible_moves.remove("right")
-    elif my_neck["y"] < my_head["y"]:  # my neck is below my head
-        possible_moves.remove("down")
-    elif my_neck["y"] > my_head["y"]:  # my neck is above my head
-        possible_moves.remove("up")
-    
-    return possible_moves    
-    
-def choose_move_basic(data: dict) -> str:
-    """
-    data: Dictionary of all Game Board data as received from the Battlesnake Engine.
-    For a full example of 'data', see https://docs.battlesnake.com/references/api/sample-move-request
-    
-    return: A String, the single move to make. One of "up", "down", "left" or "right".
-    
-    Use the information in 'data' to decide your next move. The 'data' variable can be interacted
-    with as a Python Dictionary, and contains all of the information about the Battlesnake board
-    for each move of the game.
-    
-    """
-    board = data['board']
-    my_snake = data["you"]  # A dictionary describing your snake's position on the board
-    my_head = my_snake["head"]  # A dictionary of coordinates
-    
-    possible_moves = ["up", "down", "left", "right"]
-    # Step 0: Don't allow your Battlesnake to move back on it's own neck.
-    possible_moves = _avoid_my_neck(my_snake["body"], possible_moves)
-
-    # Step 1 - Don't hit walls.
-    # Use information from `data` and `my_head` to not move beyond the game board.
-    if my_head["y"] == 0:
-        possible_moves = _rem_dir("down", possible_moves)
-    elif my_head["y"] == board['height'] - 1:
-        possible_moves = _rem_dir("up", possible_moves)
-    if my_head["x"] == 0:
-        possible_moves = _rem_dir("left", possible_moves)
-    elif my_head["x"] == board['width'] - 1:
-        possible_moves = _rem_dir("right", possible_moves)
-    
-    # Step 2 - Don't hit yourself.
-    # Use information from `my_body` to avoid moves that would collide with yourself.
-    # Step 3 - Don't collide with others.
-    # Use information from `data` to prevent your Battlesnake from colliding with others.
-    for s in board['snakes']:
-        for b in s['body']:
-            if abs(b["x"] - my_head["x"]) + abs(b["y"] - my_head["y"]) == 1:
-                if b["x"] == my_head["x"] - 1:
-                    possible_moves = _rem_dir("left", possible_moves)
-                elif b["x"] == my_head["x"] + 1:
-                    possible_moves = _rem_dir("right", possible_moves)
-                elif b["y"] == my_head["y"] - 1:
-                    possible_moves = _rem_dir("down", possible_moves)
-                elif b["y"] == my_head["y"] + 1:
-                    possible_moves = _rem_dir("up", possible_moves)
-
-    # Step 4 - Find food.
-    # Use information in `data` to seek out and find food.
-    food = board['food']
-    dist = 9999
-    nearestFood = None
-    for f in food:
-        f_dist = abs(f["x"] - my_head["x"]) + abs(f["y"] - my_head["y"])
-        if f_dist < dist:
-            dist = f_dist
-            nearestFood = f
-
-    # Choose a random direction from the remaining possible_moves to move in, and then return that move
-    move = random.choice(possible_moves)
-    if nearestFood != None:
-        if nearestFood["x"] < my_head["x"] and "left" in possible_moves:
-            move = "left"
-        elif nearestFood["x"] > my_head["x"] and "right" in possible_moves:
-            move = "right"
-        elif nearestFood["y"] < my_head["y"] and "down" in possible_moves:
-            move = "down"
-        elif nearestFood["y"] > my_head["y"] and "up" in possible_moves:
-            move = "up"        
-    
-    print(f"MOVE {data['turn']}: {move} picked from all valid options in {possible_moves}")
-        
-    return move
-    
-def choose_move(data: dict) -> str:
-    """
-    data: Dictionary of all Game Board data as received from the Battlesnake Engine.
-    For a full example of 'data', see https://docs.battlesnake.com/references/api/sample-move-request
-    
-    return: A String, the single move to make. One of "up", "down", "left" or "right".
-    
-    Use the information in 'data' to decide your next move. The 'data' variable can be interacted
-    with as a Python Dictionary, and contains all of the information about the Battlesnake board
-    for each move of the game.
-    
-    """
-    board = data['board']
-    my_head = data["you"]["head"]
-    
-    # Uncomment the lines below to see what this data looks like in your output!
-    #print(f"~~~ Turn: {data['turn']}  Game Mode: {data['game']['ruleset']['name']} ~~~")
-    #print(f"All board data this turn: {data}")
-    #print(f"My Battlesnakes head this turn is: {my_head}")
-
+def _score_food(board, scores):
     grid = np.zeros((board['width'], board['height']))
     
     for f in board['food']:
         grid[f['x']][f['y']] = -1
+
+    print("Food")
+    print(grid)
+    scores['food'] = grid
+
+def _score_snakes(board, my_head, scores):
+    grid = np.zeros((board['width'], board['height']))
+
     for s in board['snakes']:
         s_body = s['body']
                 
@@ -273,25 +54,59 @@ def choose_move(data: dict) -> str:
             grid[b['x']][b['y']] = bodyLoc
             bodyLoc -= 1
     
-    #print(grid)
-    path = findFood(grid, my_head, data['you']['body'][-1])
-    #print(path)
-    if path == None or len(path) < 2:
-        return choose_move_basic(data)
+    print("Snakes")
+    print(grid)
+    scores['snakes'] = grid
+
+def setupGrid(data: dict):
+    board = data['board']
     
-    firstNode = path[1]
-    #print(firstNode)
-    if firstNode[0] < my_head["x"]:
-        move = "left"
-    elif firstNode[0] > my_head["x"]:
-        move = "right"
-    elif firstNode[1] < my_head["y"]:
-        move = "down"
-    elif firstNode[1] > my_head["y"]:
-        move = "up"
-    else:
-        move = None
+    scores = {}
+    threds = []
+    threds.append(threading.Thread(target=_score_food, args=(board, scores)))
+    threds.append(threading.Thread(target=_score_snakes, args=(board, data["you"]["head"], scores)))
+    #_score_food(board, scores)
+    #_score_snakes(board, data["you"]["head"], scores)
+    for t in threds:
+        t.start()
+
+    for t in threds:
+        t.join()
     
-    print(f"MOVE {data['turn']}: {move} picked")
+    grid = sum(scores.values())
+    return grid
+    
+def choose_move(data: dict) -> str:
+    """
+    data: Dictionary of all Game Board data as received from the Battlesnake Engine.
+    For a full example of 'data', see https://docs.battlesnake.com/references/api/sample-move-request
+    
+    return: A String, the single move to make. One of "up", "down", "left" or "right".
+    
+    Use the information in 'data' to decide your next move. The 'data' variable can be interacted
+    with as a Python Dictionary, and contains all of the information about the Battlesnake board
+    for each move of the game.
+    
+    """
+    # Uncomment the lines below to see what this data looks like in your output!
+    print(f"~~~ Turn: {data['turn']}  Game Mode: {data['game']['ruleset']['name']} ~~~")
+    #print(f"All board data this turn: {data}")
+    my_head = data["you"]["head"]
+    #print(f"My Battlesnakes head this turn is: {my_head}")
+    
+    grid = setupGrid(data)
+    print(grid)
+
+    move = None
+    curValue = None
+    for new_position in [(0, -1, "down"), (0, 1, "up"), (-1, 0, "left"), (1, 0, "right")]: # Adjacent squares
+        # Get node position
+        pos = (my_head["x"] + new_position[0], my_head["y"] + new_position[1])
+        gridValue = grid[pos[0]][pos[1]]
+        if curValue == None or gridValue < curValue:
+            curValue = gridValue
+            move = new_position[2]
+    
+    print(f"MOVE {data['turn']}: {move} picked\n")
 
     return move
